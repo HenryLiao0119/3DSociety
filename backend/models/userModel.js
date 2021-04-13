10ce02e3b4dda736eab3005a925d6cae589734e7
Chan Li Liao <cliao119@gmail.com>
2021-04-13 16:04:24 -0400

    finished user modal

---
+++ backend/models/userModel.js
@@ -0,0 +1,32 @@
+// import for model
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+// need to use bcrypt to create hash password later
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
