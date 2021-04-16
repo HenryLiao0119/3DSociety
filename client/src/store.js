e081feb4b28fc35d70752a707c1ce375b702e05d
Chan Li Liao <cliao119@gmail.com>
2021-04-16 00:04:20 -0400

    comments on starting redux store

--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,22 @@
+// redux import
 import { createStore, applyMiddleware } from 'redux';
+
+// middleware import
 import thunk from 'redux-thunk';
+
+// redux dev tool import
 import { composeWithDevTools } from 'redux-devtools-extension';
+
+// reducer file
 import rootReducer from './reducers/index';
 
+// initialstate
 const initialState = {};
 
+// initialzed middleware
 const middleware = [thunk];
 
+// create the store
 const store = createStore(
   rootReducer,
   initialState,
