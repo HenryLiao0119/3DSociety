03bc38fa549953ac65588547979c3365bd7a4884
Chan Li Liao <cliao119@gmail.com>
2021-04-17 19:15:01 -0400

    redux workflow created

---
+++ client/src/actions/productAction.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+} from '../constants/productTypes';
+
+// list products
+
+// get single product
+
+// delete product
+
+// update product
+
+// create product
---
+++ client/src/reducers/productReducers.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+} from '../constants/productTypes';
+
+// product list
+
+// product detail
+
+// delete product
+
+// create product
+
+// update productc
