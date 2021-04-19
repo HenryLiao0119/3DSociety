c6bb6bdc83eeddf629b427fee33c90878384a5e8
Chan Li Liao <cliao119@gmail.com>
2021-04-19 18:04:39 -0400

    user login,auth,and reg backend done routes done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,10 +6,13 @@ import generateToken from '../utils/generateToken.js';
 //@route  POST /api/users/login
 //@access Public
 const authUser = asyncHandler(async (req, res) => {
+  // grab data from body
   const { email, password } = req.body;
 
+  // check user
   const user = await User.findOne({ email });
 
+  // confirm user and send the token to login
   if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
@@ -24,4 +27,63 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+//@desc   Get user profile
+//@route  GET /api/users/profile
+//@access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  // find user
+  const user = await User.findById(req.user._id);
+
+  // if found send back the user info
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+//@desc   Register a new user
+//@route  POST /api/users/
+//@access Public
+const registerUser = asyncHandler(async (req, res) => {
+  // grab the data that was send here through req.body
+  const { name, email, password } = req.body;
+
+  // check if it exist
+  const userExists = await User.findOne({ email });
+
+  // if it is,send error
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  // else make a new user
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  // send the new user back with token to login
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
+export { authUser, getUserProfile, registerUser };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -30,15 +30,16 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-// //
-// userSchema.pre('save', async function (next) {
-//   if (!this.isModified('password')) {
-//     next();
-//   }
+//encrypt the password before save
+userSchema.pre('save', async function (next) {
+  // only if is modified
+  if (!this.isModified('password')) {
+    next();
+  }
 
-//   const salt = await bcrypt.genSalt(10);
-//   this.password = await bcrypt.hash(this.password, salt);
-// });
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model('User', userSchema);
 
