5fd36e199bda4e07d842781ce80f6b4d858d3c6e
Chan Li Liao <cliao119@gmail.com>
2021-04-20 17:51:41 -0400

    change button size

--- client/src/screens/ShippingScreen.js
+++ client/src/screens/ShippingScreen.js
@@ -71,7 +71,7 @@ const ShippingScreen = ({ history }) => {
           ></Form.Control>
         </Form.Group>
 
-        <Button type='submit' variant='primary'>
+        <Button type='submit' variant='primary' className='btn-block'>
           Continue
         </Button>
       </Form>
