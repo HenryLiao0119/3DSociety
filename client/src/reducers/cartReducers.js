8a228ed299bd32e2645c4adfc230db7c7bcacff2
Chan Li Liao <cliao119@gmail.com>
2021-04-21 15:54:44 -0400

    R/A done for payment

--- client/src/reducers/cartReducers.js
+++ client/src/reducers/cartReducers.js
@@ -45,6 +45,11 @@ export default (state = initialState, action) => {
         shippingAddress: action.payload,
       };
     // save payment method
+    case CART_SAVE_PAYMENT_METHOD:
+      return {
+        ...state,
+        paymentMethod: action.payload,
+      };
     default:
       return state;
   }
