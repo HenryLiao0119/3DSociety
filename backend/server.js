8c3e540316cd2cd760267e4f4b465c49e2c01a0e
Chan Li Liao <cliao119@gmail.com>
2021-04-22 19:29:06 -0400

    fix backend bug

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import dotenv from 'dotenv';
 // import for routes
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderRoutes';
+import orderRoutes from './routes/orderRoutes.js';
 
 // import for middleware
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
