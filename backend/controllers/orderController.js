2c5c1e5d878974a848c439ef33757c354a4bde49
Chan Li Liao <cliao119@gmail.com>
2021-04-23 14:14:17 -0400

    update order to pay endpoint done

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -54,10 +54,34 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 });
 
+//@desc   update order to paid
+//@route  PUT /api/orders/:id/pay
+//@access Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updateOrder = await order.save();
+    res.json(updateOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order Not Found');
+  }
+});
+
 export {
   addOrderItems,
   getOrderById,
-  // updateOrderToPaid,
+  updateOrderToPaid,
   // getMyOrders,
   // getOrders,
   // updateOrderToDelivered,
