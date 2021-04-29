3aafc71c070c45a7c6678147a59081375742b31b
Chan Li Liao <cliao119@gmail.com>
2021-04-29 17:05:25 -0400

    search endpoint done

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -8,23 +8,22 @@ const getProducts = asyncHandler(async (req, res) => {
   // const pageSize = 2;
   // const page = Number(req.query.pageNumber) || 1;
   // this is how to get the question mark
-  // const keyword = req.query.keyword
-  //   ? {
-  //       name: {
-  //         $regex: req.query.keyword,
-  //         $options: 'i',
-  //       },
-  //     }
-  //   : {};
-  // const count = await Product.countDocuments({ ...keyword });
-  // const products = await Product.find({ ...keyword })
-  //   .limit(pageSize)
-  //   .skip(pageSize * (page - 1));
-  // res.json({ products, page, pages: Math.ceil(count / pageSize) });
-
-  const products = await Product.find({});
-
-  res.json(products);
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: 'i',
+        },
+      }
+    : {};
+  const count = await Product.countDocuments({ ...keyword });
+  const products = await Product.find({ ...keyword });
+  // .limit(pageSize)
+  // .skip(pageSize * (page - 1));
+  res.json({
+    products,
+    //page, pages: Math.ceil(count / pageSize)
+  });
 });
 
 //@desc   Fetch single product
