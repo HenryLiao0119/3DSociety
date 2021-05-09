c6ec28223233976663c005f4dc820fad278b7e6b
Chan Li Liao <cliao119@gmail.com>
2021-05-08 23:17:05 -0400

    comments for middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
 import asyncHandler from 'express-async-handler';
 
+// create a middleware to check protected route
 const protect = asyncHandler(async (req, res, next) => {
   let token;
 
@@ -9,6 +10,7 @@ const protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
+    // checks token
     try {
       token = req.headers.authorization.split(' ')[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -28,6 +30,7 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
+// create middleware to check admin routes
 const admin = (req, res, next) => {
   if (req.user && req.user.isAdmin) {
     next();
