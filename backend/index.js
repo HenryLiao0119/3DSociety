f2ead43b51fecc8c83b1f174379a2a1c7b36fa73
Henry Liao <95271550+HenryLiao0119@users.noreply.github.com>
2022-01-06 13:43:37 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -46,17 +46,17 @@ app.get('/api/config/paypal', (req, res) => {
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-// // production set up
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '/client/build')));
+ // production set up
+ if (process.env.NODE_ENV === 'production') {
+   app.use(express.static(path.join(__dirname, '/client/build')));
 
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//   );
-// } else {
-//   app.get('/', (req, res) => {
-//     res.send('API is running....');
-//   });
+   app.get('*', (req, res) =>
+   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+   );
+ } else {
+   app.get('/', (req, res) => {
+     res.send('API is running....');
+   });
 // }
 
 // error route need to be at the end
